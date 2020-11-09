f1be960a5404478e0ae2e009aa379540d54b8246
Gooossiping <georgekuo100@gmail.com>
2020-11-09 18:57:44 +0800

    File input fully completed

--- main.cpp
+++ main.cpp
@@ -10,4 +10,25 @@ int main(int argc, char* argv[]){
         cout<<"Error:Failed to open input file."<<endl;
         return 1;
     }
+
+    //parameters initialize
+    int row, col, battery;
+    Infile>>row>>col>>battery;
+    char** map = new char*[row];
+    for(int i = 0; i < row; i++){
+        map[i] = new char[col];
+    }
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++){
+            Infile>>map[i][j];
+        }
+    }
+    //Test:print the map
+    /*for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++){
+            cout<<map[i][j]<<" ";
+        }
+        cout<<endl;
+    }*/
+    
 }
